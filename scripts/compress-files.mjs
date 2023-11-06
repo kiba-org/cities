e6c5a6c59c3edf2b0e8a239e5f6df5307799b849
Hakim Gueye <beatsjordy@gmail.com>
2023-11-06 09:30:46 +0000

    separer copy et compress function different fichier

--- scripts/compress-files.mjs
+++ scripts/compress-files.mjs
@@ -1,4 +1,3 @@
-import * as fs from "fs";
 import path from "path";
 import { createReadStream, createWriteStream } from "fs";
 import { createGzip, constants } from "zlib";
@@ -69,22 +68,3 @@ outputML.on("finish", () => {
 outputML.on("error", (err) => {
   console.error("Erreur lors de la compression du fichier ML :", err);
 });
-
-/**
- * Copy files data ---> dist
- */
-const sourceDirectory = path.join(currentDir, "../data"); // Répertoire source
-const targetDirectory = path.join(currentDir, "../dist"); // Répertoire cible
-
-// Lister tous les fichiers avec l'extension .txt dans le répertoire source
-fs.readdirSync(sourceDirectory).forEach((file) => {
-  if (file.endsWith(".gz") || file === "regions.txt") {
-    const sourceFile = path.join(sourceDirectory, file);
-    const targetFile = path.join(targetDirectory, file);
-    fs.copyFileSync(sourceFile, targetFile);
-  }
-});
-
-console.log(
-  'Fichiers .txt copiés avec succès depuis le répertoire "data" vers le répertoire "dist".'
-);
